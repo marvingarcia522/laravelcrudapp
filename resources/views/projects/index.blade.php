655ce112d22e55814ebd8911d2f40de254c24132
Marvin B. Garcia <70933119+marvingarcia522@users.noreply.github.com>
2021-08-05 18:49:08 +0800

    Update index.blade.php

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -4,12 +4,14 @@
 @section('content')
 
 
+    
     <div class="row">
-        <div class="col-lg-12 margin-tb mt-1">
-            <div class="pull-left">
-                
+        <div class="col-lg-12 margin-tb">
+        <div class="pull-left mt-5">
+                <h2>List of Accomplishments</h2>
             </div>
-            <div class="pull-right">
+            <div class="pull-right mt-5" id="non-printable" >
+                
                 <a class="btn btn-success" href="{{ route('projects.create') }}" title="Add a Reoort"> <i class="fas fa-plus-square"></i>
                     </a>
             </div>
@@ -22,19 +24,32 @@
         </div>
     @endif
 
-    <table class="table table-bordered table-responsive-lg mt-1.5">
-            <thead class="thead-dark">
+    <table class="table table-bordered table-hover mt-1.5" >
+            <thead class="thead-dark"style="text-align:center">
 
-    <div class="row">
+    <div class="row" >
         <tr>
             <th scope="col">No</th>
             <th scope="col">Name</th>
-            <th scope="col">Accomplishment</th>
-            <th scope="col">Rating</th>
+            <th scope="col" >Accomplishment</th>
+            <th colspan="4" scope="colgroup">Rating</th>
             <th scope="col">Remarks</th>
             <th scope="col">Date</th>
-            <th width="120px">Action</th>
+            <th scope="col" width="120px">Action</th>
 
+        </tr>
+        <tr>
+            <th rowspan="2"></th>
+            <th rowspan="2"></th>
+            <th rowspan="2"></th>
+                <th scope="col">Q1</th>
+                <th scope="col">E2</th>
+                <th scope="col" >T3</th>
+                <th scope="col">A4</th>
+                <th rowspan="2"></th>
+            <th rowspan="2"></th>
+            <th rowspan="2"></th>
+           
         </tr>
         <tbody>
         @foreach ($projects as $project)
@@ -42,7 +57,10 @@
             <td>{{ ++$i }}</td>
             <td>{{ $project->name }}</td>
             <td>{{ $project->accomplishment }}</td>
-            <td>{{ $project->ranking }}</td>
+            <td>{{ $project->rating}}</td>
+            <td>{{ $project->rating}}</td>
+            <td>{{ $project->rating}}</td>
+            <td>{{ $project->rating}}</td>
             <td> {{ $project->remarks }}</td>
             <td>{{ date_format($project->created_at, 'jS M Y') }}</td>
             <td>
@@ -71,6 +89,7 @@
         </tbody>
         @endforeach
         </table>
+        <div id="non-printable" ><button onClick="window.print()">Print this page  <i class="fa fa-print" aria-hidden="true"></i></button></div>
 
 
     {!! $projects->links() !!}
